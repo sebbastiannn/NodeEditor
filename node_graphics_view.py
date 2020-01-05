99623664f0fd9a200f488db2fa04a17d13d2947c
sebbastiannn <59449017+sebbastiannn@users.noreply.github.com>
2020-01-05 21:18:23 +0100

    create dragging edge

--- node_graphics_view.py
+++ node_graphics_view.py
@@ -2,6 +2,15 @@ from PyQt5.QtWidgets import QGraphicsView
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
+from node_graphics_socket import QDMGraphicsSocket
+
+
+MODE_NOOP = 1
+MODE_EDGE_DRAG = 2
+
+EDGE_DRAG_START_THRESHOLD = 10
+
+
 class QDMGraphicsView(QGraphicsView):
     def __init__(self, grScene, parent=None):
         super().__init__(parent)
@@ -10,6 +19,8 @@ class QDMGraphicsView(QGraphicsView):
         self.initUI()
 
         self.setScene(self.grScene)
+
+        self.mode = MODE_NOOP
 
         self.zoomInFactor = 1.25
         self.zoomClamp = True
@@ -29,49 +40,12 @@ class QDMGraphicsView(QGraphicsView):
         # set the Center under the Mouse for scrolling in/out
         self.setTransformationAnchor(QGraphicsView.AnchorUnderMouse)
 
-    '''
-    def mousePressEvent(self, event):
-        if event.button() == Qt.MiddleButton:
-            self.middleMouseButtonPress(event)
-        elif event.button() == Qt.LeftButton:
-            self.rightMouseButtonPress(event)
-        elif event.button() == Qt.RightButton:
-            self.rightMouseButtonPress(event)
-        else:
-            super().mousePressEvent(event)
-
-    def mouseReleaseEvent(self, event):
-        if event.button() == Qt.MiddleButton:
-            self.middleMouseButtonRelease(event)
-        elif event.button() == Qt.LeftButton:
-            self.leftMouseButtonRelease(event)
-        elif event.button() == Qt.RightButton:
-            self.rightMouseButtonRelease(event)
-        else:
-            super().mouseReleaseEvent(event)
-
-
-    def middleMouseButtonPress(self, event):
-        releaseEvent = QMouseEvent(QEvent.MouseButtonRelease, event.localPos(), event.screenPos(),
-                                   Qt.LeftButton, Qt.NoButton, event.modifiers())
-        super().mouseReleaseEvent(releaseEvent)
-        self.setDragMode(QGraphicsView.ScrollHandDrag)
-        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
-                                Qt.LeftButton, event.buttons() | Qt.LeftButton, event.modifiers())
-        super().mousePressEvent(fakeEvent)
-
-    def middleMouseButtonRelease(self, event):
-        fakeEvent = QMouseEvent(event.type(), event.localPos(), event.screenPos(),
-                                Qt.LeftButton, event.buttons() & ~Qt.LeftButton, event.modifiers())
-        super().mouseReleaseEvent(fakeEvent)
-        self.setDragMode(QGraphicsView.NoDrag)
-    ''' #irgendwas geht in dem Code nicht obwohl dieser identisch ist
 # Drag around
     def mousePressEvent(self, event):
         if event.button() == Qt.MiddleButton:
             self.middleMouseButtonPress(event)
         elif event.button() == Qt.LeftButton:
-            self.rightMouseButtonPress(event)
+            self.leftMouseButtonPress(event)
         elif event.button() == Qt.RightButton:
             self.rightMouseButtonPress(event)
         else:
@@ -107,16 +81,70 @@ class QDMGraphicsView(QGraphicsView):
 
 
     def leftMouseButtonPress(self, event):
-        return super().mousePressEvent(event)
+        # get item which we clicked on
+        item = self.getItemAtClick(event)
+
+        # we store the position of last LMB click
+        self.last_lmb_click_scene_pos = self.mapToScene(event.pos())
+
+        # logic
+        if type(item) is QDMGraphicsSocket:
+            if self.mode == MODE_NOOP:
+                self.mode = MODE_EDGE_DRAG
+                self.edgeDragStart(item)
+                return
+
+        if self.mode == MODE_EDGE_DRAG:
+            res = self.edgeDragEnd(item)
+            if res: return
+
+        super().mousePressEvent(event)
 
     def leftMouseButtonRelease(self, event):
-        return super().mouseReleaseEvent(event)
+        # get item which we release mouse button on
+        item = self.getItemAtClick(event)
+
+        # logic
+        if self.mode == MODE_EDGE_DRAG:
+            if self.distanceBetweenClickAndReleaseIsOff(event):
+                res = self.edgeDragEnd(item)
+                if res: return
+
+        super().mouseReleaseEvent(event)
 
     def rightMouseButtonPress(self, event):
-        return super().mousePressEvent(event)
+        super().mousePressEvent(event)
 
     def rightMouseButtonRelease(self, event):
-        return super().mouseReleaseEvent(event)
+        super().mouseReleaseEvent(event)
+
+    def getItemAtClick(self, event):
+        """ return the object on which we've clicked/release mouse button """
+        pos = event.pos()
+        obj = self.itemAt(pos)
+        return obj
+
+    def edgeDragStart(self, item):
+        print('Start dragging edge')
+        print('  assign Start Socket')
+
+    def edgeDragEnd(self, item):
+        """ return True if skip the rest of the code """
+        self.mode = MODE_NOOP
+        print('End dragging edge')
+
+        if type(item) is QDMGraphicsSocket:
+            print('  assign End Socket')
+            return True
+
+        return False
+
+    def distanceBetweenClickAndReleaseIsOff(self, event):
+        """ measures if we are too far from the last LMB click scene position """
+        new_lmb_release_scene_pos = self.mapToScene(event.pos())
+        dist_scene = new_lmb_release_scene_pos - self.last_lmb_click_scene_pos
+        edge_drag_threshold_sq = EDGE_DRAG_START_THRESHOLD * EDGE_DRAG_START_THRESHOLD
+        return (dist_scene.x() * dist_scene.x() + dist_scene.y() * dist_scene.y()) > edge_drag_threshold_sq
 
     def wheelEvent(self, event):
         # calculate our zoom factor
